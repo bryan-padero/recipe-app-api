0eee8af46a8cf69c634b06155552a12a50a86ff3
Bryan <bryanpadero@gmail.com>
2022-10-20 22:00:29 +0400

    Updated admin to support custom user model.

--- app/core/models.py
+++ app/core/models.py
@@ -23,6 +23,7 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user models that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
